0914632d2cdc1d51721aeda557a4f75b051cdb90
jaredianmills <jaredianmills@gmail.com>
2018-05-10 21:54:21 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,9 +137,9 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-      
+
       board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-      
+
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
